4ac0c5e91585264b3c57a14d9880da9c9a65eb2a
David Taylor <david@sabo.tech>
2023-01-08 10:25:48 +0100

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,71 @@
+import discord
+from discord import Message as DiscordMessage
+import logging
+from src.base import Message, Conversation
+from src.constants import (
+    BOT_INVITE_URL,
+    DISCORD_BOT_TOKEN,
+    EXAMPLE_CONVOS,
+    ACTIVATE_THREAD_PREFX,
+    MAX_THREAD_MESSAGES,
+    SECONDS_DELAY_RECEIVING_MSG,
+)
+import asyncio
+from src.utils import (
+    logger,
+    should_block,
+    close_thread,
+    is_last_message_stale,
+    discord_message_to_message,
+)
+from src import completion
+from src.completion import generate_completion_response, process_response
+from src.moderation import (
+    moderate_message,
+    send_moderation_blocked_message,
+    send_moderation_flagged_message,
+)
+
+logging.basicConfig(
+    format="[%(asctime)s] [%(filename)s:%(lineno)d] %(message)s", level=logging.INFO
+)
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+tree = discord.app_commands.CommandTree(client)
+
+
+@client.event
+async def on_ready():
+    logger.info(f"We have logged in as {client.user}. Invite URL: {BOT_INVITE_URL}")
+    completion.MY_BOT_NAME = client.user.name
+    completion.MY_BOT_EXAMPLE_CONVOS = []
+    for c in EXAMPLE_CONVOS:
+        messages = []
+        for m in c.messages:
+            if m.user == "Lenard":
+                messages.append(Message(user=client.user.name, text=m.text))
+            else:
+                messages.append(m)
+        completion.MY_BOT_EXAMPLE_CONVOS.append(Conversation(messages=messages))
+    await tree.sync()
+
+# Checks if the bot was mentioned and returns the asnwer
+@client.event
+async def on_message(message):
+    if client.user in message.mentions:
+        # generate the response
+        async with message.channel.typing():
+            response_data = await generate_completion_response(
+                messages=message, user=message.author
+            )
+
+        # send response
+        await generate_completion_response(
+            user=message.author, channel=message.channel, response_data=response_data
+        )
+
+
+client.run(DISCORD_BOT_TOKEN)
